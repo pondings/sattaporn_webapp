3f7b83a8819d64230ddce7f3069fd121c1dcbc66
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-07 19:14:01 +0700

    [Component.ts] (customer-form-modal.component.ts) : update code ;

--- src/app/customer/customer-form-modal/customer-form-modal.component.ts
+++ src/app/customer/customer-form-modal/customer-form-modal.component.ts
@@ -1,6 +1,6 @@
 import { CustomerService } from './../../shared/services/customer.service';
 import { Customer } from './../../shared/models/Customer';
-import { Component, OnInit, ViewChild, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter, Output, Input, ElementRef, Renderer } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { saveAs } from 'file-saver';
 import 'rxjs/Rx';
@@ -17,7 +17,12 @@ import { Modal } from 'clarity-angular';
 })
 export class CustomerFormModalComponent implements OnInit {
 
+  @ViewChild('fileUploader') fileUploader: ElementRef;
   @ViewChild('customerFormModal') customerformModal: Modal;
+  @ViewChild('submitBtn') submitBtn: ElementRef;
+  @ViewChild('cancelBtn') cancelBtn: ElementRef;
+  @ViewChild('downloadBtn') downloadBtn: ElementRef;
+  @ViewChild('uploadBtn') uploadBtn: ElementRef;
   @Output() customerModel: EventEmitter<Customer> = new EventEmitter();
   @Output() updatedCustomer: EventEmitter<Customer> = new EventEmitter();
   private viewMode: Boolean;
@@ -25,8 +30,11 @@ export class CustomerFormModalComponent implements OnInit {
   private opened: boolean;
   private customerFormModalHeader: string;
   private customer: Customer;
+  private uploadStatus: string;
+  private uploadAble: Boolean = true;
 
-  constructor(private fb: FormBuilder, private customerService: CustomerService) {
+  constructor(private fb: FormBuilder, private customerService: CustomerService, private elementRef: ElementRef,
+      private renderer: Renderer) {
     this.createForm();
   }
 
@@ -58,14 +66,24 @@ export class CustomerFormModalComponent implements OnInit {
         rs => this.updatedCustomerEmit(rs, this.customer.index),
         error => console.log(error)
       );
-    }else {
+    } else {
       this.customerService.createCustomer(form).subscribe(rs => this.customerEmit(rs), error => console.log(error));
     }
   }
 
   public fileChange(event) {
     const fileList: FileList = event.target.files;
-    this.customerService.uploadDocument(this.customer, fileList).subscribe(rs => this.customer = rs, error => console.log(error));
+    this.disableAllButton();
+    this.customerService.uploadDocument(this.customer, fileList).subscribe(
+      (rs) => {
+        this.customer.document1 = rs.document1;
+        this.enableAllButton();
+      },
+      (error) => {
+        this.enableAllButton();
+        alert('Can not upload file pls contract Pawarut Klaiarmon');
+      }
+    );
   }
 
   public downloadDocument() {
@@ -78,20 +96,19 @@ export class CustomerFormModalComponent implements OnInit {
 
   public customerEmit(customer: any) {
     this.customerModel.emit(customer);
-    this.closeModal();
+    this.customer = customer;
+    this.enterViewMode();
   }
 
   public updatedCustomerEmit(customer: any, index: number) {
     const updatedCustomer: Customer = customer;
     updatedCustomer.index = this.customer.index;
-    console.log(index);
     this.updatedCustomer.emit(updatedCustomer);
-    this.closeModal();
+    this.enterViewMode();
   }
 
   private editButtonClicked() {
-    this.viewMode = false;
-    this.form.enable();
+    this.enterEditMode();
   }
 
   public openModal() {
@@ -101,15 +118,23 @@ export class CustomerFormModalComponent implements OnInit {
 
   public createCustomer() {
     this.opened = true;
-    this.customer = null;
     this.customerFormModalHeader = 'Create Customer';
-    this.viewMode = false;
-    this.form.enable();
+    this.enterCreateMode();
   }
 
   public viewCustomerInfo(customer: Customer) {
     this.opened = true;
     this.customerFormModalHeader = 'View Customer Info';
+    this.setCustomer(customer);
+    this.enterViewMode();
+  }
+
+  private openFileUploader() {
+    this.fileUploader.nativeElement.value = '';
+    this.fileUploader.nativeElement.click();
+  }
+
+  private setCustomer(customer: Customer) {
     this.customer = { ...customer };
     this.customer.index = customer.index;
     this.form.patchValue({
@@ -122,8 +147,40 @@ export class CustomerFormModalComponent implements OnInit {
       workAddress: customer.workAddress,
       index: customer.index
     });
+  }
+
+  private enterViewMode() {
     this.viewMode = true;
     this.form.disable();
+  }
+
+  private enterEditMode() {
+    this.viewMode = false;
+    this.form.enable();
+  }
+
+  private enterCreateMode() {
+    this.customer = null;
+    this.viewMode = false;
+    this.form.enable();
+  }
+
+  private disableAllButton() {
+    if (this.downloadBtn) {
+      this.downloadBtn.nativeElement.disabled = true;
+    }
+    this.uploadBtn.nativeElement.disabled = true;
+    this.submitBtn.nativeElement.disabled = true;
+    this.cancelBtn.nativeElement.disabled = true;
+  }
+
+  private enableAllButton() {
+    if (this.downloadBtn) {
+      this.downloadBtn.nativeElement.disabled = false;
+    }
+    this.uploadBtn.nativeElement.disabled = false;
+    this.submitBtn.nativeElement.disabled = false;
+    this.cancelBtn.nativeElement.disabled = false;
   }
 
   public closeModal() {
