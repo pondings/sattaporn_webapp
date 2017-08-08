fcf36c25f3aec32494c098d076bf0e137b02f3e9
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-08 21:16:38 +0700

    [Component] (document-list-modal.component) : Add documentation list modal ;

---
+++ src/app/shared/modals/document-list-modal/document-list-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-list-modal',
+  templateUrl: './document-list-modal.component.html',
+  styleUrls: ['./document-list-modal.component.scss']
+})
+export class DocumentListModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
