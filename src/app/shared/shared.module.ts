855507174361716275ef7450c55ad68ec522d9da
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-08 13:50:47 +0700

    [Module] (shared.module.ts) : Import DocumentTableComponent ;

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmationModalComponent } from './modals/confirmation-modal/confirmation-modal.component';
-
+import { DocumentListModalComponent } from './modals/document-list-modal/document-list-modal.component';
+import { DocumentTableComponent } from './tables/document-table/document-table.component';
 /**
  * Third-Party
  */
 import { ClarityModule } from 'clarity-angular';
-import { DocumentListModalComponent } from './modals/document-list-modal/document-list-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ClarityModule
   ],
-  declarations: [ConfirmationModalComponent, DocumentListModalComponent],
+  declarations: [ConfirmationModalComponent, DocumentListModalComponent, DocumentTableComponent],
   exports: [ConfirmationModalComponent, DocumentListModalComponent]
 })
 export class SharedModule { }
