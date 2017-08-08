7d61ad3b506f65f7d893f52f887ed6b46f4a14c2
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-08 13:52:54 +0700

    [Component] (customer-table.component.ts) : Change ngx-datatable to clarity data table ;

--- src/app/customer/customer-table/customer-table.component.ts
+++ src/app/customer/customer-table/customer-table.component.ts
@@ -1,10 +1,8 @@
-import { DocumentListModalComponent } from './../../shared/modals/document-list-modal/document-list-modal.component';
 import { ConfirmationModalComponent } from './../../shared/modals/confirmation-modal/confirmation-modal.component';
 import { CustomerSearchComponent } from './../customer-search/customer-search.component';
 import { CustomerService } from './../../shared/services/customer.service';
 import { Customer } from './../../shared/models/Customer';
 import { Component, OnInit, ViewChild, TemplateRef, EventEmitter, Output } from '@angular/core';
-import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-customer-table',
@@ -14,36 +12,18 @@ import { DatatableComponent } from '@swimlane/ngx-datatable';
 export class CustomerTableComponent implements OnInit {
 
   @Output() viewCustomerInfo: EventEmitter<Customer> = new EventEmitter();
-  @ViewChild('btnGroup') btnGroupTemp: TemplateRef<any>;
-  @ViewChild('phone') phoneTemp: TemplateRef<any>;
-  @ViewChild('code') codeTemp: TemplateRef<any>;
-  @ViewChild('fullName') fullNameTemp: TemplateRef<any>;
-  @ViewChild('address') addressTemp: TemplateRef<any>;
-  @ViewChild('dataTable') dataTable: DatatableComponent;
   @ViewChild(ConfirmationModalComponent) confirmationModalComponent: ConfirmationModalComponent;
-  @ViewChild(DocumentListModalComponent) documentListModalComponent: DocumentListModalComponent;
-  private rows: Customer[] = [];
+  private customerList: Customer[] = [];
   private columns;
 
   constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
     this.findAll();
-    this.columns = [
-      { prop: 'Code', cellTemplate: this.codeTemp },
-      { name: 'Customer Name', cellTemplate: this.fullNameTemp },
-      { name: 'Phone', cellTemplate: this.phoneTemp },
-      { name: 'Address', cellTemplate: this.addressTemp },
-      { name: 'Command', cellTemplate: this.btnGroupTemp, width: 80, cellClass: 'text-center' }
-    ];
-  }
-
-  public documentBtnClicked(index: number) {
-    this.documentListModalComponent.openDocumentListModalWithCustomerCode(this.rows[index]);
   }
 
   public infoBtnClicked(customer: Customer, index: number) {
-    const targetCustomer: Customer = this.rows[index];
+    const targetCustomer: Customer = this.customerList[index];
     targetCustomer.index = index;
     customer.index = index;
     this.viewCustomerInfo.emit(targetCustomer);
@@ -54,18 +34,18 @@ export class CustomerTableComponent implements OnInit {
   }
 
   public editRow(customer: Customer) {
-    let updatedCustomer: Customer = this.rows[customer.index];
+    let updatedCustomer: Customer = this.customerList[customer.index];
     updatedCustomer.fullName = customer.fullName;
     updatedCustomer.code = customer.code;
     updatedCustomer.phone = customer.phone;
     updatedCustomer.address = customer.address;
     updatedCustomer.document1 = customer.document1;
     updatedCustomer = customer;
-    this.rows[customer.index] = updatedCustomer;
+    this.customerList[customer.index] = updatedCustomer;
   }
 
   private insertUpdateRowToTop(customer: Customer) {
-    this.rows.unshift(customer);
+    this.customerList.unshift(customer);
   }
 
   public removeBtnClicked(customer: Customer, index: number) {
@@ -85,15 +65,15 @@ export class CustomerTableComponent implements OnInit {
   }
 
   public spliceTable(index: number) {
-    this.rows.splice(index, 1);
+    this.customerList.splice(index, 1);
   }
 
   public fillTable(data: Customer[]) {
-    this.rows = data;
+    this.customerList = data;
   }
 
   public addRecord(customer: Customer) {
-    this.rows.unshift(customer);
+    this.customerList.unshift(customer);
   }
 
 
