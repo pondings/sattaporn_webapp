aba79d36229b4fe6c37d701a23096ee00258eaa8
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-09 14:15:23 +0700

    [Component] (document-modal.component.ts)  : Add open & close modal method and upload file function ;

--- src/app/shared/modals/document-modal/document-modal.component.ts
+++ src/app/shared/modals/document-modal/document-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { DocumentService } from './../../services/document.service';
+import { Customer } from './../../models/Customer';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-document-modal',
@@ -7,18 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentModalComponent implements OnInit {
 
+  @ViewChild('fileUploader') fileUploader: ElementRef;
   public opened: boolean;
+  public customer: Customer;
 
-  constructor() { }
+  constructor(private documentService: DocumentService) { }
 
   ngOnInit() {
+  }
+
+  public openFileUploader() {
+    this.fileUploader.nativeElement.click();
+  }
+
+  public fileChange(event) {
+    const fileList: FileList = event.target.files;
+    this.documentService.uploadDocument(null, fileList).subscribe(
+      (rs) => {
+        console.log(rs);
+      }
+    );
+    this.fileUploader.nativeElement.value = '';
   }
 
   public openModal() {
     this.opened = true;
   }
 
-  public closeModl() {
+  public openModalWithcustomer(customer: Customer) {
+    this.opened = true;
+    this.customer = customer;
+  }
+
+  public closeModal() {
     this.opened = false;
   }
 
