e885da78352b013c437db7e91b2a6896686e3cfc
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-09 12:08:16 +0700

    [Module] (document.module.ts) : Import document search form component ;

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DocumentRoutingModule } from './document-routing.module';
 import { DocumentComponent } from './document.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /**
  * Third-Party
  */
 import { ClarityModule } from 'clarity-angular';
+import { DocumentSearchFormComponent } from './document-search-form/document-search-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DocumentRoutingModule,
-    ClarityModule
+    ClarityModule,
+    ReactiveFormsModule
   ],
-  declarations: [DocumentComponent]
+  declarations: [DocumentComponent, DocumentSearchFormComponent]
 })
 export class DocumentModule { }
