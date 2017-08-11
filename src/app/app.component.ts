a9a1b02fe7761670900b23ddb3d3b4b852081eb4
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-12 01:03:32 +0700

    import ngx-translate and set default i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+/**
+ * Third-Party
+ */
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
 }
