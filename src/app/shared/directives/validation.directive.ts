94cca8d6fe03825530e7569f3446271aa96cce99
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-12 17:24:44 +0700

    Customer validation direct show validation message ;

--- src/app/shared/directives/validation.directive.ts
+++ src/app/shared/directives/validation.directive.ts
@@ -1,5 +1,11 @@
+import { CustomValidation } from './../services/custom-validation.service';
 import { Directive, ElementRef, Renderer, HostListener, DoCheck, Input } from '@angular/core';
 import { NgControl } from '@angular/forms';
+
+/**
+ * Third-Party
+ */
+import { TranslateService } from '@ngx-translate/core';
 
 @Directive({
   selector: 'input[appValidation]'
@@ -9,23 +15,30 @@ export class ValidationDirective {
   @Input() showErrorMessage = true;
   @Input() isDisabled = false;
   private smallElement: any;
+  private parentElement: ElementRef;
   private message: any;
   private parentNode: any;
 
-  constructor(private el: ElementRef, private renderer: Renderer, private control: NgControl) { }
+  constructor(private el: ElementRef, private renderer: Renderer, private control: NgControl, private translate: TranslateService) {
+    this.smallElement = this.renderer.createElement(this.el.nativeElement.parentNode, 'small');
+    renderer.setElementStyle(this.smallElement, 'color', 'red');
+    renderer.setElementStyle(this.smallElement, 'display', 'none');
+    this.message = this.renderer.createText(this.smallElement, '');
+
+  }
 
   ngDoCheck() {
-
+    (this.hasErrors && this.isTouched) ? this.showsmallElement() : this.hidesmallElement();
   }
 
   private getsmallElement(): string {
     for (const propertyName in this.control.errors) {
       if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
-        // const msg: string;
-        // return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
-        // this.translate.get('Validation.' + propertyName, { value: this.control.errors[propertyName] })
-        //   .subscribe((rs: string) => msg = rs);
-        // return msg;
+        let msg: string;
+        // return CustomValidation.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
+        this.translate.get('Validation.' + propertyName, { value: this.control.errors[propertyName] })
+          .subscribe((rs: string) => msg = rs);
+        return msg;
       }
     }
     return null;
@@ -43,12 +56,10 @@ export class ValidationDirective {
     const displayMessage: string = this.showErrorMessage ? 'block' : 'none';
     this.renderer.setText(this.message, this.getsmallElement());
     this.renderer.setElementStyle(this.smallElement, 'display', displayMessage);
-    this.renderer.setElementClass(this.parentNode, 'has-danger', true);
   }
 
   private hidesmallElement() {
     this.renderer.setElementStyle(this.smallElement, 'display', 'none');
-    this.renderer.setElementClass(this.parentNode, 'has-danger', false);
   }
 
 }
