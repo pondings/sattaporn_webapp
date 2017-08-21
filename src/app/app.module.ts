9ee2fb24efcf2a9d5fe689cfc0442c4d90db3f5a
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-22 01:19:15 +0700

    Restore ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, Http } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
