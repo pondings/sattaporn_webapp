23904df15e102ece3d03961c963b0f104fe767e8
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-21 23:28:06 +0700

    Edit validation directive

--- src/app/shared/directives/validation.directive.ts
+++ src/app/shared/directives/validation.directive.ts
@@ -1,5 +1,5 @@
 import { CustomValidation } from './../services/custom-validation.service';
-import { Directive, ElementRef, Renderer, HostListener, DoCheck, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, DoCheck, Input, AfterViewInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 /**
@@ -10,28 +10,69 @@ import { TranslateService } from '@ngx-translate/core';
 @Directive({
   selector: 'input[appValidation]'
 })
-export class ValidationDirective {
+export class ValidationDirective implements DoCheck, AfterViewInit {
 
   @Input() showErrorMessage = true;
   @Input() isDisabled = false;
-  private smallElement: any;
-  private parentElement: ElementRef;
-  private message: any;
-  private parentNode: any;
+  @Input() tooltipPos: string;
+  private labelElement: ElementRef;
+  private spanElement: ElementRef;
+  private message: string;
 
   constructor(private el: ElementRef, private renderer: Renderer, private control: NgControl, private translate: TranslateService) {
-    this.smallElement = this.renderer.createElement(this.el.nativeElement.parentNode, 'small');
-    renderer.setElementStyle(this.smallElement, 'color', 'red');
-    renderer.setElementStyle(this.smallElement, 'display', 'none');
-    this.message = this.renderer.createText(this.smallElement, '');
+    this.spanElement = renderer.createElement(el.nativeElement.parentNode, 'span');
+    this.labelElement = renderer.createElement(el.nativeElement.parentNode, 'label');
 
+    // Render element style class
+    renderer.setElementClass(this.spanElement, 'tooltip-content', true);
+    renderer.setElementClass(this.labelElement, 'tooltip', true);
+    renderer.setElementClass(this.labelElement, 'tooltip-validation', true);
+    renderer.setElementStyle(this.labelElement, 'z-index', '999');
+
+    // Put span & input into label
+    renderer.projectNodes(this.labelElement, [el.nativeElement, this.spanElement]);
+    this.message = this.renderer.createText(this.spanElement, '');
   }
 
+  /**
+   * Override
+   */
+  ngAfterViewInit(): void {
+    let toolTipPosition = '';
+    switch (this.tooltipPos) {
+      case 'top-right':
+        toolTipPosition = 'tooltip-top-right';
+        break;
+      case 'top-left':
+        toolTipPosition = 'tooltip-top-left';
+        break;
+      case 'bottom-right':
+        toolTipPosition = 'tooltip-bottom-right';
+        break;
+      case 'bottom-left':
+        toolTipPosition = 'tooltip-bottom-left';
+        break;
+      case 'right':
+        toolTipPosition = 'tooltip-right';
+        break;
+      case 'left':
+        toolTipPosition = 'tooltip-left';
+        break;
+      default:
+        toolTipPosition = 'tooltip-top-right';
+        break;
+    }
+    this.renderer.setElementClass(this.labelElement, toolTipPosition, true);
+  }
+
+  /**
+   * Override
+   */
   ngDoCheck() {
-    (this.hasErrors && this.isTouched) ? this.showsmallElement() : this.hidesmallElement();
+    (this.hasErrors && this.isTouched) ? this.showspanElement() : this.hidespanElement();
   }
 
-  private getsmallElement(): string {
+  private setSpanElementMessage(): string {
     for (const propertyName in this.control.errors) {
       if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
         let msg: string;
@@ -52,14 +93,13 @@ export class ValidationDirective {
     return this.control.errors != null;
   }
 
-  private showsmallElement() {
-    const displayMessage: string = this.showErrorMessage ? 'block' : 'none';
-    this.renderer.setText(this.message, this.getsmallElement());
-    this.renderer.setElementStyle(this.smallElement, 'display', displayMessage);
+  private showspanElement() {
+    this.renderer.setElementClass(this.labelElement, 'invalid', true);
+    this.renderer.setText(this.message, this.setSpanElementMessage());
   }
 
-  private hidesmallElement() {
-    this.renderer.setElementStyle(this.smallElement, 'display', 'none');
+  private hidespanElement() {
+    this.renderer.setElementClass(this.labelElement, 'invalid', false);
   }
 
 }
