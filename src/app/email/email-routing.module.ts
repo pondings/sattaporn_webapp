91e6d2c3ff768ed18793fa67704549a88a5552cf
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-22 01:15:21 +0700

    Add routing to email component

--- src/app/email/email-routing.module.ts
+++ src/app/email/email-routing.module.ts
@@ -1,7 +1,10 @@
+import { EmailComponent } from './email.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'email', component: EmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
