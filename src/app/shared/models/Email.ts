916391ef2abbb92f5c4dcaf577062b327f76b5fd
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-22 23:50:59 +0700

    add email model

---
+++ src/app/shared/models/Email.ts
@@ -0,0 +1,11 @@
+export class Email {
+
+        public username: string;
+        public password: string;
+        public content: string;
+        public sendFrom: string;
+        public sendTo: string;
+        public subject: string;
+
+    }
+
