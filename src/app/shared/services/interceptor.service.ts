d0e63770b51366896cad423b9af6fae962500c43
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-23 16:58:32 +0700

    Add interceptor for append content type automatically and prepare to implement JWT

---
+++ src/app/shared/services/interceptor.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+
+@Injectable()
+export class InterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    let transformedReq: HttpRequest<any> = req;
+
+    if (req.headers.keys().length === 0) {
+      transformedReq = req.clone({
+        headers: req.headers.set('Content-Type', 'application/json')
+      });
+    }
+
+    return next.handle(transformedReq).map(
+      (res: HttpResponse<any>) => {
+        return res;
+      }).catch(
+        (err) => {
+          console.log('Error in request : ' + err);
+          return Observable.throw(err);
+        }
+      );
+  }
+}
