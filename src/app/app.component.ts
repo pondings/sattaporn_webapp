950291ab0606fb934b71c6363a741b7728c1ae42
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-24 01:19:39 +0700

    Set vertical nav hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 /**
  * Third-Party
  */
 import { TranslateService } from '@ngx-translate/core';
+import {VerticalNav} from '../../node_modules/clarity-angular/layout/vertical-nav/';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
-  title = 'app';
+  @ViewChild('verticalNav') verticalNav: VerticalNav;
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
     translate.use('en');
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.verticalNav.collapsed = true, 0);
   }
 
 }
