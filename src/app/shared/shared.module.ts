414957b781442fea720c764f58c51ba2a4c1c9a4
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-24 01:18:27 +0700

    Import reactive form module and other

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
+import { CustomerTableComponent } from './tables/customer-table/customer-table.component';
 import { DocumentModule } from './../document/document.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ConfirmationModalComponent } from './modals/confirmation-modal/confirmation-modal.component';
 import { DocumentTableComponent } from './tables/document-table/document-table.component';
 /**
@@ -11,15 +13,19 @@ import { DocumentModalComponent } from './modals/document-modal/document-modal.c
 import { SettingModalComponent } from './modals/setting-modal/setting-modal.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ValidatorDirective } from './directives/validator.directive';
+import { CustomerSelectorModalComponent } from './modals/customer-selector-modal/customer-selector-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ClarityModule,
     DocumentModule,
-    TranslateModule
+    TranslateModule,
+    ReactiveFormsModule
   ],
-  declarations: [ConfirmationModalComponent, DocumentTableComponent, DocumentModalComponent, SettingModalComponent, ValidatorDirective],
-  exports: [ConfirmationModalComponent, DocumentModalComponent, SettingModalComponent, ValidatorDirective]
+  declarations: [ConfirmationModalComponent, DocumentTableComponent, DocumentModalComponent, SettingModalComponent,
+     ValidatorDirective, CustomerSelectorModalComponent, CustomerTableComponent],
+  exports: [ConfirmationModalComponent, DocumentModalComponent, SettingModalComponent, ValidatorDirective, CustomerSelectorModalComponent, 
+  CustomerTableComponent]
 })
 export class SharedModule { }
