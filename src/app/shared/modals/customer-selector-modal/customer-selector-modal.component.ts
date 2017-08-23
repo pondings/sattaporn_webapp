fcfd0bb3e2f49a3be444f6d734beb6755dce4eca
Pawarut klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-24 01:17:48 +0700

    Add new component for select customer email

---
+++ src/app/shared/modals/customer-selector-modal/customer-selector-modal.component.ts
@@ -0,0 +1,66 @@
+import { CustomerService } from './../../services/customer.service';
+import { Customer } from './../../models/Customer';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-customer-selector-modal',
+  templateUrl: './customer-selector-modal.component.html',
+  styleUrls: ['./customer-selector-modal.component.scss']
+})
+export class CustomerSelectorModalComponent implements OnInit {
+
+  @Output() selectedCustomer: EventEmitter<Customer> = new EventEmitter();
+  private opened = false;
+  private customerList: Customer[] = [];
+  public form: FormGroup;
+
+  constructor(private customerService: CustomerService, private fb: FormBuilder) {
+    this.createForm();
+   }
+
+  ngOnInit() {
+    this.customerService.findAll().then(
+      (res) => this.fillTable(res)
+    );
+  }
+
+  private selectCustomer(customer: Customer) {
+    this.selectedCustomer.emit(customer);
+    this.closeModal();
+  }
+
+  private onsubmit(formValue: any) {
+    const customer: Customer = new Customer;
+    customer.searchKeyword = formValue.searchKeyword;
+    customer.findMethod = formValue.findMethod;
+    this.customerService.findCustomer(customer).then(
+      (res) => this.fillTable(res)
+    );
+  }
+
+  private resetForm() {
+    this.form.reset();
+    this.form.controls.findMethod.setValue('code');
+  }
+
+  private createForm() {
+    this.form = this.fb.group({
+      searchKeyword: '',
+      findMethod: ['code', Validators.required]
+    });
+  }
+
+  private fillTable(customerList: Customer[]) {
+    this.customerList = customerList;
+  }
+
+  public openModal() {
+    this.opened = true;
+  }
+
+  public closeModal() {
+    this.opened = false;
+  }
+
+}
