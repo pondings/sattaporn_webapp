d34d3c5e121f4f52b5882e024805e1f3d85d7573
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-23 19:26:07 +0700

    import whatever i use

--- src/app/email/email.module.ts
+++ src/app/email/email.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EmailRoutingModule } from './email-routing.module';
 import { EmailComponent } from './email.component';
 
+import { ClarityModule } from 'clarity-angular';
+import { TranslateModule} from '@ngx-translate/core';
+import { EmailFormComponent } from './email-form/email-form.component';
+import { EmailAuthenModalComponent } from './email-authen-modal/email-authen-modal.component';
+
 @NgModule({
   imports: [
     CommonModule,
-    EmailRoutingModule
+    ReactiveFormsModule,
+    EmailRoutingModule,
+    ClarityModule,
+    TranslateModule
   ],
-  declarations: [EmailComponent]
+  declarations: [EmailComponent, EmailFormComponent, EmailAuthenModalComponent]
 })
 export class EmailModule { }
