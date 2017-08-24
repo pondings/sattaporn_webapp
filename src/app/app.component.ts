ab6e594d2cd950f2f30a864668dbec433380067b
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-24 19:10:42 +0700

    hode vertical nav while click content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,31 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { UserInfoService } from './shared/services/user-info.service';
+import { Component, ViewChild, AfterViewInit, Renderer } from '@angular/core';
 
 /**
  * Third-Party
  */
 import { TranslateService } from '@ngx-translate/core';
-import {VerticalNav} from '../../node_modules/clarity-angular/layout/vertical-nav/';
+import { VerticalNav } from '../../node_modules/clarity-angular/layout/vertical-nav/';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
 
   @ViewChild('verticalNav') verticalNav: VerticalNav;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private userInfoService: UserInfoService, private renderer: Renderer) {
     translate.setDefaultLang('en');
     translate.use('en');
   }
 
   ngAfterViewInit() {
-    setTimeout(() => this.verticalNav.collapsed = true, 0);
+  }
+
+  public clickedBody() {
+    this.verticalNav.collapsed = true;
   }
 
 }
