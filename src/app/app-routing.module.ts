fe4ad82bbf428f06c437f0e739ba1c2aa3202610
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-25 16:44:39 +0700

    not match path redirect to /dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { AuthGuardService } from './shared/services/auth-guard.service';
 import { NotFoundComponent } from './pages/authenication/not-found/not-found.component';
-import { LogoutComponent } from './pages/authenication/logout/logout.component';
 import { LoginComponent } from './pages/authenication/login/login.component';
 import { EmailComponent } from './pages/email/email.component';
 import { DocumentComponent } from './pages/document/document.component';
@@ -16,9 +15,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService] },
   { path: 'email', component: EmailComponent, canActivate: [AuthGuardService] },
   { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent },
   { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
